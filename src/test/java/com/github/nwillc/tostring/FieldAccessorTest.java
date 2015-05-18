f3a7aa90beb3a810ae6598c8de525fa6063244f9
N Christopher <nwillc@gmail.com>
2015-05-18 15:49:49 -0400

    Tests improved

--- src/test/java/com/github/nwillc/tostring/FieldAccessorTest.java
+++ src/test/java/com/github/nwillc/tostring/FieldAccessorTest.java
@@ -1,10 +1,14 @@
 package com.github.nwillc.tostring;
 
 import com.github.nwillc.contracts.UtilityClassContract;
+import org.assertj.core.api.Condition;
+import org.assertj.core.description.Description;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,10 +46,28 @@ public class FieldAccessorTest extends UtilityClassContract {
     public void testFieldStream() throws Exception {
         Stream fields = FieldAccessor.get(instance);
         assertThat(fields).isNotNull();
-        assertThat(fields.count()).isEqualTo(2);
+        Object[] entries = fields.toArray();
+        assertThat(entries.length).isEqualTo(2);
+        assertThat(entries).haveAtLeastOne(new EntryMapCondition("str", "foo"));
+        assertThat(entries).haveAtLeastOne(new EntryMapCondition("x", 5));
     }
 
-    private final static class Sample {
+    private static class EntryMapCondition extends Condition<Object> {
+        final String key;
+        final Object value;
+
+        public EntryMapCondition(String key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public boolean matches(Object o) {
+            Map.Entry<String,Object> entry = (Map.Entry<String, Object>) o;
+            return entry.getKey().equals(key) && entry.getValue().equals(value);
+        }
+    }
+    private static class Sample {
         private final String str;
         private final int x;
 
